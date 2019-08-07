fcd3a818e036aae6f6ef21ae5af5efdd222f7659
Mohamed Abdulle <yarae99@gmail.com>
2019-08-07 05:07:39 -0500

    menu collapse feature

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-x=0
+screenWidth=$(window).width()
+item=0
 list=['images/img1a.jpg','images/img1b.jpg','images/img1c.jpg']
+bigScreen= undefined
+brek= 650
+call=$('.info p:last')
+
 setInterval(function(){
-    $('.wrapper').css({'background': `url(${list[x]}) no-repeat`, 'background-size': 'cover  '})
-    x++
-    if (x>2){x=0}
+    $('.wrapper').css({'background': `url(${list[item]}) no-repeat`, 'background-size': 'cover  '})
+    item++
+    if (item>2){item=0}
 },2500)
+
+function diffsize(x){ //piece of genius
+    if(screenWidth>brek && bigScreen!=true){
+        console.log('big screen')
+         $('ul').slideDown()
+         $('.menu .info-p').fadeOut(0)
+        bigScreen=true
+    }
+    else if(screenWidth<brek && bigScreen!=false){
+        console.log('small screen')
+        $('.menu').prepend(call.clone())
+        $('ul').slideUp(0)
+        $('.fa-bars').unbind('click').click(function(){$('ul').slideToggle();console.log(x)})
+        bigScreen=false
+    }
+}
+
 function animator(ele1,ele2,pos,clas){
     if($(window).scrollTop()>=$(ele1).offset().top-($(window).height()*pos)){
         $(ele2).addClass(clas)
     }
 }
+
+diffsize('one')
 
 $(window).scroll(function(){
     animator('.about','.about img',(1/2),'aboutImg')
@@ -20,4 +44,11 @@ $(window).scroll(function(){
         animator(`.sev:eq(${i})`,`.sev:eq(${i}) p`,(3/4),'sevP')
     }
     
+})
+
+$(window).resize(function(){
+    screenWidth=$(window).width()
+
+    diffsize('two')
+
 })
